349bdb80f4ad3ea6b00c7d2a6099ec9c74dadbca
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-05-27 23:36:45 +0100

    feat(main): add the entry of the rest api

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+(C)Copyright 2023, PROJECT
+Guy Ahonakpon GBAGUIDI
+"""
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.controllers import auth
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.AUTH)
+
+
+@app.get("/")
+def read_main():
+    """default function"""
+    return {"message": "Welcome to CLEANCOMM REST API development"}
